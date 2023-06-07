de58d55dcbbbd0b6b077c5da5ec2ebacaefc8b36
TopperClausen <thomas.clausen14@gmail.com>
2023-06-07 12:56:34 +0200

    added home link to nav

--- styles-frontend/src/components/Navigator.vue
+++ styles-frontend/src/components/Navigator.vue
@@ -3,7 +3,10 @@ import { RouterLink } from "vue-router";
 </script>
 
 <template>
-    <nav class="h-12 px-4 shadow mb-5 bg-white">
+    <nav class="h-12 px-4 shadow mb-5 bg-white flex justify-between align-middle">
+        <div class="flex flex-row h-full items-center">
+            <RouterLink class="text-xl text-gray-500 hover:text-black" to="/"> Styles </RouterLink>
+        </div>
         <ul class="flex flex-row-reverse h-full items-center">
             <li>
                 <router-link class="bg-gray-200 py-2 px-1 rounded bg-opacity-60 hover:bg-opacity-80" to="/styles"> Redigér styles </router-link>
